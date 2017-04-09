06dc767f238509d8f493ccebc330801e3f1a5130
mohkof1106 <mohkof1106@gmail.com>
2017-04-09 14:31:11 +0400

    Revert "HM"
    This reverts commit 5ae11676a134a80def7f321421ae377229679703.

--- chronograph/__init__.py
+++
@@ -1,3 +0,0 @@
-VERSION = (0, 3, 2, 'dev')
-
-__version__ = '.'.join((str(each) for each in VERSION[:4]))
--- status.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python ~/modbus_controller/manage.py control --pythonpath=~/modbus_controller --daemon=status
